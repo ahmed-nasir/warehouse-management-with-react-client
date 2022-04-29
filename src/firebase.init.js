725202c04f1fe6a32eb0e1615905e8e045c9a9a6
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-29 17:15:29 +0600

    add env file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7zcg8StQYFnOkAJhmsuffLCZ6yPGw7D8",
-  authDomain: "warehouse-management-e1fee.firebaseapp.com",
-  projectId: "warehouse-management-e1fee",
-  storageBucket: "warehouse-management-e1fee.appspot.com",
-  messagingSenderId: "948448258131",
-  appId: "1:948448258131:web:d883257560eb268a66fea6"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
