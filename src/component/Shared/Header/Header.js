969cf7b6cc260bf1d4dab1a0dfbcb940c7176ecb
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-30 17:33:57 +0600

    if login show logout btn

--- src/component/Shared/Header/Header.js
+++ src/component/Shared/Header/Header.js
@@ -1,8 +1,12 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+  const [user]=useAuthState(auth);
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -11,7 +15,12 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+            {
+              user? 
+              <button className='btn btn-primary' onClick={()=>signOut(auth)}>Log out</button>
+              :
+              <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+            }
 
 
           </Nav>
