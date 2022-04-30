3cede29318f99bd0d68aadb4c90485d8def9ae9a
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-30 18:02:51 +0600

    Design log in page

--- src/component/Login/Login/Login.js
+++ src/component/Login/Login/Login.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate=useNavigate();
+
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    
+    if(user){
+        navigate('/')
+    }
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        await signInWithEmailAndPassword(email, password);
+        
+       
+    }
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to='/register'>Goto SignUp</Link>
-        </div>
+        <div className='container w-50 mx-auto'>
+            
+            <h2 className='text-primary text-center mt-2'>Please Login</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+                    Login
+                </Button>
+            </Form>
+            
+            <p>New to warehouse? <Link to="/register" className='text-primary pe-auto text-decoration-none' >Please Register</Link> </p>
+            <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' >Reset Password</button> </p>
+           </div>
     );
 };
 
